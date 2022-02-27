31fdc75c90d09a49551ea574771b871f7309a2c4
Agon <68818772+Agon-Ademaj@users.noreply.github.com>
2022-02-27 13:24:51 +0100

    update header.php
    Perditesimit i header-it ne folderin include

--- include/header.php
+++ include/header.php
@@ -1,18 +1,18 @@
 <header class="header">
     <nav class="nav-bar">
         <div class="logo">
-            <h1 class="pointer" style="cursor:pointer;">Virtual</h1>
+            <h1 class="pointer" style="cursor:pointer;">AgoX</h1>
         </div>
         <button class="hamburger" id="hamburger">
             <i class="fas fa-bars"></i>
         </button>
         <ul class="nav-ul" id="nav-ul">
-            <li><a class="nav-list" href="../virtual/index.php">Home</a></li>
-            <li><a class="nav-list" href="#">Shop</a></li>
-            <li><a class="nav-list" href="#">About</a></li>
-            <li><a class="nav-list" href="#">Leadership</a></li>
-            <li><a class="nav-list" href="#">Register</a></li>
-            <li><a class="nav-list" href="#">Log In</a></li>
+            <li><a class="nav-list" href="../virtual/index.php">Kryefaqja</a></li>
+            <li><a class="nav-list" href="../virtual/products.php">Dyqani</a></li>
+            <li><a class="nav-list" href="../virtual/about.php">Rreth nesh</a></li>
+            <li><a class="nav-list" href="../virtual/leadership.php">Stafi</a></li>
+            <li><a class="nav-list" href="../virtual/register.php">Regjistrohu</a></li>
+            <li><a class="nav-list" href="../virtual/logIn.php">Kyqu</a></li>
         </ul>
     </nav>
 </header>
