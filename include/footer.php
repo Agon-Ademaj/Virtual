23c28082d032e6a44a504f02fa2ba5985b86d947
Agon <68818772+Agon-Ademaj@users.noreply.github.com>
2022-02-27 13:25:37 +0100

    Update footer.php
    Perditesimit i footer-it ne folderin include

--- include/footer.php
+++ include/footer.php
@@ -2,12 +2,12 @@
     <div class="footer__container">
         <div class="footer__row">
             <div class="foo footer__one">
-                <h4>About Us</h4>
+                <h4>Rreth Nesh</h4>
                 <hr size=".5">
-                <p>Stratus is a powerful WordPress theme designed for app, SaaS, & tech startups, including all the important features you need to make your business successful.</p>
+                <p>AgoX është një nga kompanitë e shërbimeve të internetit. Kompani kjo e cila në vazhdimësi po rritet në menyrë eksponenciale vit pas viti. AgoX është kompani lidere e shitjes online.</p>
             </div>
             <div class="foo footer__two">
-                <h4>Payments Accepted</h4>
+                <h4>Pagesat te cilat pranohen</h4>
                 <hr size=".5">
                 <button class="btn_square">PayPal</button>
                 <button class="btn_square">Visa</button>
@@ -16,14 +16,14 @@
         </div>
         <div class="footer__row">
             <div class="foo footer__three">
-                <h4>Contact Info</h4>
+                <h4>Kontakti</h4>
                 <hr size=".5">
                 <p>contact@thevirtualkosova.com</p>
                 <p>038100100</p>
                 <p>Location</p>
             </div>
             <div class="footer_four">
-                <h4>Be Social</h4>
+                <h4>Rrjetet sociale</h4>
                 <hr size=".5">
                 <p>Facebook</p>
                 <p>Twitter</p>
