955f0471ee5b2ee35566e4a2e8fa662ae186894f
Agon <68818772+Agon-Ademaj@users.noreply.github.com>
2022-02-27 13:23:28 +0100

    update index.html

--- index.php
+++ index.php
@@ -1,23 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<style>
+    html {
+    scroll-behavior: smooth;
+}
 </style>
 
 <head>
     <title>VR Stratus</title>
     <link rel="stylesheet" href="css/index.css">
 
-    <!-- Importimi i stilizimit te shkronjave -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;1,300&family=Quicksand:wght@300;400&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 
 </head>
 
 <body>
 
-    <?php include 'include/header.php' ?>
+    <?php include '../Virtual/include/header.php' ?>
 
     <div class="container">
         <!-- Section 1 -->
@@ -25,10 +29,10 @@
             <img class="img" src="img/prod2_crop.hero.jpg" alt="">
 
             <div class="home__container">
-                <h1>Immerse Yourself</h1>
+                <h1>Madhështia nuk mbaron kurrë,<br>por evulon</h1>
                 <div class="buttons">
-                    <a href="#features" class="btn btn__blue">Explore</a>
-                    <a href="#" class="btn btn__white">Buy Now</a>
+                    <a href="#features" class="btn btn__blue">Eksploro</a>
+                    <a href="Products.php" class="btn btn__white">Bleni tani</a>
                 </div>
             </div>
         </div>
@@ -37,7 +41,7 @@
         <div class="slider">
             <div class="slider__content">
                 <img class="icon" src="img/EyeIcon.png" alt="">
-                <h2>Unlike anything you've ever experienced</h2>
+                <h2>Ndryshe nga çdo gjë që keni përjetuar ndonjëherë</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel turpis sed felis finibus scelerisque. Sed et facilisis urna. Vestibulum vehicula scelerisque pellentesque. Suspendisse at dolor leo. Integer ac luctus nisi, a scelerisque odio. Integer non nisl eu ligula semper dictum.</p>
             </div>
             <img class="virtual_showcase" src="img/prod2_showcase.jpg" alt="">
@@ -47,7 +51,7 @@
         <div class="features" id="features">
             <div class="features__header">
                 <img class="icon" src="img/flash.png" alt="">
-                <h2>Next-generation virtual reality</h2>
+                <h2>Realiteti virtual i gjeneratës së ardhshme</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel turpis sed felis finibus scelerisque. Sed et facilisis urna. Vestibulum vehicula scelerisque pellentesque. Suspendisse at dolor leo.</p>
             </div>
 
@@ -56,24 +60,24 @@
                     <div class="row">
                         <div class="feature feature__one">
                             <img class="icons" src="img/square.png" alt="">
-                            <h3>Mobile Interface</h3>
+                            <h3>Ndërfaqja celulare</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida leo porttitor pretium ullamcorper. Fusce consectetur turpis nec dolor laoreet ultrices. In a venenatis leo.</p>
                         </div>
                         <div class="feature feature__two">
                             <img class="icons" src="img/camera.png" alt="">
-                            <h3>User Experience</h3>
+                            <h3>Eksperienca e perdoruesit</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida leo porttitor pretium ullamcorper. Fusce consectetur turpis nec dolor laoreet ultrices. In a venenatis leo.</p>
                         </div>
                     </div>
                     <div class="row">
                         <div class="feature feature__three">
                             <img class="icons" src="img/mobile2.png" alt="">
-                            <h3>Web Development</h3>
+                            <h3>Zhvillimi i Uebit</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida leo porttitor pretium ullamcorper. Fusce consectetur turpis nec dolor laoreet ultrices. In a venenatis leo.</p>
                         </div>
                         <div class="feature feature__four">
                             <img class="icons" src="img/arrow.png" alt="">
-                            <h3>Project Strategy</h3>
+                            <h3>Strategjia e Projektit</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida leo porttitor pretium ullamcorper. Fusce consectetur turpis nec dolor laoreet ultrices. In a venenatis leo.</p>
                         </div>
                     </div>
@@ -88,8 +92,9 @@
         <!-- Section 4 -->
         <div class="theme">
             <div class="theme__container">
-                <h3>Is Stratus a good fit for you? Buy it now on ThemeForest!</h3>
-                <button class="btn btn__white">Buy Theme</button>
+                <h3>A janë produktet tona të përshtatshme për ju? Bleni tani në dyqanin tonë online!</h3>
+                <!-- <button class="btn btn__white">Bleni ketu</button> -->
+                <a href="products.php" class="btn btn__white">Bleni këtu</a>
             </div>
         </div>
 
@@ -98,27 +103,27 @@
             <div class="advantages_all">
                 <div class="advantages__header">
                     <img class="icon" src="img/settings.png" alt="">
-                    <h2>Rebuilt from the inside out</h2>
+                    <h2>Ndërtim cilësor si nga jashtë, ashtu dhe brenda</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel turpis sed felis finibus scelerisque. Sed et facilisis urna. Vestibulum vehicula scelerisque pellentesque. Suspendisse at dolor leo.</p>
                 </div>
                 <div class="advanteges__content">
                     <div class="advantages__content_left">
-                        <img class="riftrear__image" src="img/prod2_showcase2.jpg" alt="">
+                        <img class="riftrear__image" src="img/inside.png" alt="">
                     </div>
                     <div class="advantages__content_right">
                         <div class="advantages advantages__one">
                             <img class="icons" src="img/square__grey.png" alt="">
-                            <h3>Exquisite Design</h3>
+                            <h3>Dizajn i hollë</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida leo porttitor pretium ullamcorper.</p>
                         </div>
                         <div class="advantages advantages__two">
                             <img class="icons" src="img/camera_grey.png" alt="">
-                            <h3>Fully Customizable</h3>
+                            <h3>Plotësisht i personalizueshëm</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida leo porttitor pretium ullamcorper.</p>
                         </div>
                         <div class="advantages__three">
                             <img class="icons" src="img/unlimited.png" alt="">
-                            <h3>Fully Customizable</h3>
+                            <h3>Përmirësime të pakufizuara</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida leo porttitor pretium ullamcorper.</p>
                         </div>
                     </div>
@@ -131,7 +136,7 @@
                 <img class="innovative__image" src="img/prod2_tour.jpg" alt="">
             </div>
             <div class="innovaive__container">
-                <h2>Innovative controllers to match</h2>
+                <h2>Kontrollues inovatorë për përshtatje</h2>
                 <p>Maecenas aliquet massa tempus, euismod ipsum id, tempus ante. Maecenas vulputate nibh et nulla euismod, egestas bibendum ante molestie. Integer metus libero, vulputate dictum libero vel, iaculis bibendum neque. Vestibulum id imperdiet lorem. Maecenas sollicitudin ex nunc. In hac habitasse platea dictumst. Mauris magna urna, gravida quis tempus sodales, lobortis varius lacus. Ut lacinia, leo eget suscipit mattis.</p>
             </div>
         </div>
@@ -140,15 +145,15 @@
 
         <div class="riviews">
             <div class="riviews__container">
-                <h3>"A groundbreaking design that delivers on its promises and exceeds expectations- even with the bar set high. This truly feels like the future of gaming."</h3>
+                <h3>"Një dizajn inovator që përmbush premtimet e tij dhe tejkalon pritshmëritë. Kjo ndjehet vërtet si e ardhmja e lojërave."</h3>
                 <img class="testimonial" src="img/testimonial.jpg" alt="">
                 <p>Sandra Thompson</p>
-                <p>Tech Reviewer</p>
+                <p>Rishikues Teknik</p>
             </div>
         </div>
     </div>
 
-    <?php include 'include/footer.php' ?>
+    <?php include '../Virtual/include/footer.php' ?>
 
     <script src="js/script.js"></script>
 </body>
